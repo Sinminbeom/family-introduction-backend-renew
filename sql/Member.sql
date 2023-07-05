f5f1e82d15ca6ac997ca4455e503fade7b9e4aa6
mbshin <mbshin@swm.ai>
2023-07-05 14:05:57 +0900

    feat: #6 파일 업로드 추가
    파일 업로드 추가
    
    close #6

--- sql/Member.sql
+++
@@ -1,9 +0,0 @@
-create TABLE User
-(
-    id bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-    name varchar(255),
-    password varchar(255),
-    email varchar(255),
-    image LONGBLOB,
-    primary key (id)
-);
