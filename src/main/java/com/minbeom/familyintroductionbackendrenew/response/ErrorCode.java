a6686aad43d3b9e8c230d544b90e6abe7b419e19
minbeom <alsqja17@naver.com>
2023-07-26 00:04:09 +0900

    feat: #21 커스텀 예외처리 추가
    커스텀 예외처리 추가
    
    close #21

--- src/main/java/com/minbeom/familyintroductionbackendrenew/response/ErrorCode.java
+++ src/main/java/com/minbeom/familyintroductionbackendrenew/response/ErrorCode.java
@@ -1,8 +1,9 @@
 package com.minbeom.familyintroductionbackendrenew.response;
 
 public enum ErrorCode {
-    INVALID_PARAMETER(400, null, "Invalid Request Data"),
-    VALIDATE_DUPLICATE_USER(401, "U001", "이미 존재하는 유저입니다");
+    INVALID_PARAMETER(400, null, "유효하지 않은 데이터입니다"),
+    VALIDATE_DUPLICATE_USER(401, "U001", "이미 존재하는 유저입니다"),
+    INVALID_PASSWORD_USER(402, "U002", "비밀번호가 틀렸습니다.");
 
     private final int status;
     private final String code;
